ab157dc99afb61fcfc14503e737582b5c2e36b8b
andrewyk99 <andrewyk99@hotmail.com>
2022-07-19 23:34:26 -0600

    allowed posts to be created, edited, and deleted

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -26,4 +26,28 @@ router.get('/newpost', withAuth, (req, res) => {
     res.render('newpost', { loggedIn: true });
 });
 
+router.get('/:id', withAuth, (req, res) => {
+    Post.findByPk(req.params.id , {
+        attributes: [
+            'id',
+            'title',
+            'post_content',
+            'created_at'
+        ]
+    }).then(dbPostData => {
+        if (dbPostData) {
+            const post = dbPostData.get({ plain: true });
+
+            res.render('singlepost', {
+                post,
+                loggedIn: true
+            });
+        } else {
+            res.status(404).end();
+        }
+    }).catch(err => {
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
