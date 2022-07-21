e827c1745e2701f8cf5a60eec402adbcea04b76c
andrewyk99 <andrewyk99@hotmail.com>
2022-07-21 17:19:44 -0600

    allowed user to create a comment on already existing posts

--- models/Comment.js
+++ models/Comment.js
@@ -15,7 +15,7 @@ Comment.init(
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
-                len: [4]
+                len: [1]
             }
         },
         user_id: {
@@ -27,7 +27,7 @@ Comment.init(
         },
         post_id: {
             type: DataTypes.INTEGER,
-            allowNull: false,
+            allowNull: true,
             references: {
                 model: 'post',
                 key: 'id'
